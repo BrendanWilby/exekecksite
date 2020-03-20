99aa111e683679e5fe111ba367b8dbe2809e5986
BrendanWilby <brendan.wilby@gmail.com>
2020-03-20 12:44:20 +0000

    first commit

---
+++ keckdatabase/views.py
@@ -0,0 +1,38 @@
+from django.views import generic
+from django.shortcuts import render
+from keckdatabase.models import Object, Measurements, Observations
+
+def index(request):
+    num_objects = Object.objects.all().count()
+    object_list = Object.objects.all()
+
+    context = {
+        'num_objects' : num_objects,
+        "home_page" : "active"
+    }
+
+    return render(request, 'index.html', context=context)
+
+class ObjectListView(generic.ListView):
+    model = Object
+
+    def get_context_data(self, **kwargs):
+        context = super(ObjectListView, self).get_context_data(**kwargs)
+        context['measurement'] = Measurements.objects.all()
+        context['observation'] = Observations.objects.all()
+        context["list_page"] = "active"
+
+        return context
+
+class ObjectDetailView(generic.DetailView):
+    model = Object
+    context_object_name = "object-detail"
+
+    def get_context_data(self, **kwargs):
+        context = super(ObjectDetailView, self).get_context_data(**kwargs)
+        ob_id = self.kwargs['pk']
+        context['measurement'] = Measurements.objects.get(mid=ob_id)
+        context['observation'] = Observations.objects.get(obid=ob_id)
+        context["list_page"] = "active"
+
+        return context
